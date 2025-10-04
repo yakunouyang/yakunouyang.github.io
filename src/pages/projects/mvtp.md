2a0d2bc638904e60af7060df72d2a114d84f24c2
Yakun Ouyang <85840949+yakunouyang@users.noreply.github.com>
2025-10-04 16:18:56 +0800

    intial commit

---
+++ src/pages/projects/mvtp.md
@@ -0,0 +1,4 @@
+---
+title: Trajectory Planning for Multiple Vehicles
+
+---
---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		fontFamily: {
+      sans: ['SF Pro Text', 'Helvetica Neue', 'Inter', 'Helvetica', 'Arial', 'sans-serif'],
+    },
+		extend: {},
+	},
+	plugins: [],
+}
